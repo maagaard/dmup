383561ec5c1ca985cac4824288aafed1b09406cc
Emil Maagaard <emaagaard@gmail.com>
2014-10-20 13:47:22 +0200

    add setup.py - not finished

---
+++ Project/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+# import main_module
+
+setup(
+	name= #of package
+	version= # main_module.__version__,
+	author=
+	author_email=
+	description= # main_module.__doc__,
+	license='MIT',	#include LICENSE-MIT or something like that
+	keywords=''
+	url='http://github.com/maagaard/dmup/project'
+	packages=find_packages(),
+	install_requirements=[],
+	tests_requires=[],
+	classifiers=[
+		'Programming Language :: Python :: 2.7',
+		'Licence :: OSI Approved :: MIT License'
+	],
+	
+
+)
