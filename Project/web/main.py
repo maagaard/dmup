fc3a67e0252538260f5773dbbd4e6ef38537f44a
Bjarke Vad <bjarke.vad90@gmail.com>
2014-10-22 13:31:32 +0200

    cleanup

--- Project/web/main.py
+++ Project/web/main.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class NavigationItem:
+    def __init__(self, href, caption):
+        self.href = href
+        self.caption = caption
+
+
 @app.route('/')
 def main_page():
-    return render_template('mainpage.html')
+    return render_template('mainpage.html',
+                           navigation=[NavigationItem("HREF", "CAPTION")])
 
 if __name__ == '__main__':
     # app.debug = True
