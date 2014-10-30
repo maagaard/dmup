94206fad600fd02f8e7002f00f67fe675c3512b5
Bjarke Vad <bjarke.vad90@gmail.com>
2014-10-30 15:27:26 +0100

    showing tweets on webui

--- Project/web/main.py
+++ Project/web/main.py
@@ -1,5 +1,10 @@
+import sys
+sys.path.insert(0, '../')
+
 from flask import Flask, render_template
 from collections import namedtuple
+# from model import Tweet
+from twitter import get_timeline
 
 app = Flask(__name__)
 
@@ -15,8 +20,14 @@ NavigationItem = namedtuple("NavigationItem", ['href', 'caption'])
 @app.route('/')
 def main_page():
     return render_template('mainpage.html',
-                           navigation=[NavigationItem("HREF", "CAPTION")])
+                           tweets=get_timeline("liverpool"))
+
+
+@app.route('/hashtag/<hashtag>')
+def tweets(hashtag=""):
+    return render_template('mainpage.html',
+                           tweets=get_timeline(hashtag))
 
 if __name__ == '__main__':
     # app.debug = True
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
