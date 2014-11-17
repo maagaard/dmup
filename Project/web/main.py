8b900d2739f6dc8d77d09a17e31f1e235e314758
Bjarke Vad <bjarke.vad90@gmail.com>
2014-11-17 16:43:18 +0100

    search input

--- Project/web/main.py
+++ Project/web/main.py
@@ -1,4 +1,5 @@
 import sys
+from application_only_auth import ClientException
 sys.path.insert(0, '../')
 
 from flask import Flask, render_template, request
@@ -8,21 +9,29 @@ from twitter import get_timeline
 
 app = Flask(__name__)
 
+NavigationItem = namedtuple('NavigationItem', ['href', 'caption'])
+navigation_items = [
+    NavigationItem('#', 'Home'),
+    NavigationItem('top', 'Top Tweets'),
+    NavigationItem('about', 'About'),
+    ]
 
-# class NavigationItem:
-#     def __init__(self, href, caption):
-#         self.href = href
-#         self.caption = caption
 
-NavigationItem = namedtuple("NavigationItem", ['href', 'caption'])
-
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main_page():
-    hashtag = request.args.get('hashtag', '')
-    return render_template('mainpage.html',
-                           tweets=get_timeline(hashtag, 10)
-                           if hashtag else [])
+    hashtag = request.form['hashtag'] if request.method == 'POST' else request.args.get('hashtag', '')
+
+    print "Querying hashtag: " + hashtag
+
+    try:
+        tweets = get_timeline(hashtag, 10) if hashtag else []
+        return render_template('mainpage.html',
+                               tweets=tweets,
+                               navigationitems=navigation_items)
+    except ClientException:
+        # Too many request, show something...
+        return render_template('mainpage.html')
+
 
 if __name__ == '__main__':
     # app.debug = True
