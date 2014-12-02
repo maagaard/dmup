82207694772bfb88e085da6622d187a5b383ec5e
Bjarke Vad <bjarke.vad90@gmail.com>
2014-12-02 14:01:09 +0100

    More database operations

--- Project/database/init.py
+++ Project/database/init.py
@@ -34,7 +34,72 @@ def create_tables(connection):
 
     Tables are created on the database in connection"""
 
+    cur = connection.cursor()
     # should switch from datatype 'json' to 'jsonb' when using Postgres >9.4
     # and apply GIN for better query performance
-    execute_sql(connection,
-                "CREATE TABLE tweets (id serial, polarity double precision, tweetdata json)")
+    cur.execute("""
+    CREATE TABLE tweets
+    (
+      id serial NOT NULL,
+      created timestamp with time zone,
+      polarity double precision,
+      data json,
+      CONSTRAINT tweets_pkey PRIMARY KEY (id)
+    )
+    WITH (
+      OIDS=FALSE
+    );
+    """)
+
+    cur.execute("""
+    CREATE TABLE hashtags
+    (
+      id serial NOT NULL,
+      hashtag character varying(139),
+      polarity double precision,
+      CONSTRAINT hashtags_pkey PRIMARY KEY (id),
+      CONSTRAINT hashtags_hashtag_key UNIQUE (hashtag)
+    )
+    WITH (
+      OIDS=FALSE
+    );
+    """)
+
+    cur.execute("CREATE INDEX hashtag_idx ON hashtags(hashtag)")
+
+    cur.execute("""
+    CREATE TABLE tweet_hashtag
+    (
+      tweet_id integer NOT NULL,
+      hashtag_id integer NOT NULL,
+      CONSTRAINT tweet_hashtag_pkey PRIMARY KEY (tweet_id, hashtag_id),
+      CONSTRAINT tweet_hashtag_hashtag_id_fkey FOREIGN KEY (hashtag_id)
+          REFERENCES hashtags (id) MATCH SIMPLE
+          ON UPDATE NO ACTION ON DELETE NO ACTION,
+      CONSTRAINT tweet_hashtag_tweet_id_fkey FOREIGN KEY (tweet_id)
+          REFERENCES tweets (id) MATCH SIMPLE
+          ON UPDATE NO ACTION ON DELETE NO ACTION
+    )
+    WITH (
+      OIDS=FALSE
+    );
+    """)
+
+
+    connection.commit()
+    cur.close()
+
+
+def create_tweet(connection, tweet):
+    for hashtag in tweet['entities']['hashtags']:
+        text = hashtag['text']
+        print text
+    raise NotImplementedError
+
+
+def read_tweets_hashtag(connection, hashtag):
+    raise NotImplementedError
+
+
+def read_tweets_date(connection, from_date, to_date):
+    raise NotImplementedError
